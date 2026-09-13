ea667e5dfb61137d06e64823ab3b2bfb8dad3150
VlaDosick <Mlgtrack@yandex.ru>
2026-09-13 03:27:59 +0300

    fixCheckStyle

--- src/main/java/ru/practicum/shareit/request/service/ItemRequestService.java
+++ src/main/java/ru/practicum/shareit/request/service/ItemRequestService.java
@@ -1,11 +1,15 @@
 package ru.practicum.shareit.request.service;
 
 import ru.practicum.shareit.request.dto.ItemRequestDto;
+
 import java.util.Collection;
 
 public interface ItemRequestService {
     ItemRequestDto create(Long requestorId, ItemRequestDto dto);
+
     Collection<ItemRequestDto> findByRequestor(Long requestorId);
+
     Collection<ItemRequestDto> findAll();
+
     ItemRequestDto findById(Long id);
 }
--- src/main/java/ru/practicum/shareit/request/service/ItemRequestServiceImpl.java
+++ src/main/java/ru/practicum/shareit/request/service/ItemRequestServiceImpl.java
@@ -7,6 +7,7 @@ import ru.practicum.shareit.request.model.ItemRequest;
 import ru.practicum.shareit.request.repository.ItemRequestRepository;
 import ru.practicum.shareit.user.model.User;
 import ru.practicum.shareit.user.repository.UserRepository;
+
 import java.time.LocalDateTime;
 import java.util.Collection;
 import java.util.stream.Collectors;
